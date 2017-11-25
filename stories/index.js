cb2e4cdfde32fe830a0c7fff7d2095b249ae2182
Thom Allen <thomwork@gmail.com>
2017-11-25 12:06:20 +0000

    Switch eslint config from airbnb to eslint-config-unobtrusive

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { withConsole } from '@storybook/addon-console';
 import Graph from '../src/';
 
